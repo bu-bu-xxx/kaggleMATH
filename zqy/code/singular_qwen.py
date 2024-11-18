354fb9a616103e2209decb2ecc78f7ca03baf5ba
bblamb_redmi <779117133@qq.com>
2024-11-18 16:24:56 +0800

    [zqy] modify 1118 hpc

--- zqy/code/singular_qwen.py
+++ zqy/code/singular_qwen.py
@@ -8,10 +8,12 @@ from time import time
 # import kaggle_evaluation.aimo_2_inference_server
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-DIR_REPO = '/mnt/kaggleMATH/'
+# DIR_REPO = '/mnt/kaggleMATH/'
+DIR_REPO = os.path.join(os.path.expanduser("~"), "learningFile", "kaggleMATH")
 repeat = 2
 model_name = "Qwen/Qwen2.5-Math-72B-Instruct"
 cache_dir = os.path.join(DIR_REPO, "zqy", "temp")
+
 
 class TransformerSolver:
     def __init__(self, model_name, device="cuda"):
@@ -27,26 +29,30 @@ class TransformerSolver:
         self.tokenizer = AutoTokenizer.from_pretrained(
             self.model_name,
             cache_dir=cache_dir,
-            )
+        )
 
     def solve(self, prompt, reasoning_type="CoT"):
         if reasoning_type == "CoT":
             messages = [
-                {"role": "system", "content": "Please reason step by step, and put your final answer within \\boxed{}."},
-                {"role": "user", "content": prompt}
+                {
+                    "role": "system",
+                    "content": "Please reason step by step, and put your final answer within \\boxed{}.",
+                },
+                {"role": "user", "content": prompt},
             ]
         elif reasoning_type == "TIR":
             messages = [
-                {"role": "system", "content": "Please integrate natural language reasoning with programs to solve the problem above, and put your final answer within \\boxed{}."},
-                {"role": "user", "content": prompt}
+                {
+                    "role": "system",
+                    "content": "Please integrate natural language reasoning with programs to solve the problem above, and put your final answer within \\boxed{}.",
+                },
+                {"role": "user", "content": prompt},
             ]
         else:
             raise ValueError("Unsupported reasoning type")
 
         text = self.tokenizer.apply_chat_template(
-            messages,
-            tokenize=False,
-            add_generation_prompt=True
+            messages, tokenize=False, add_generation_prompt=True
         )
         model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
 
@@ -55,20 +61,24 @@ class TransformerSolver:
             max_new_tokens=self.max_new_tokens,
         )
         generated_ids = [
-            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+            output_ids[len(input_ids) :]
+            for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
         ]
 
-        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[
+            0
+        ]
         return response
+
 
 # create a solver
 
-device = "cuda" # the device to load the model onto
+device = "cuda"  # the device to load the model onto
 solver = TransformerSolver(model_name, device)
 
 
 def pred2int(pred):
-    matches = re.findall(r'\\boxed{([^}]+)}', pred)
+    matches = re.findall(r"\\boxed{([^}]+)}", pred)
     tag = False
     for val in matches[-1::-1]:
         try:
@@ -83,30 +93,31 @@ def pred2int(pred):
         return 0
 
 
-id_problem_path = os.path.join(DIR_REPO, 'zqy', 'temp', 'reference.csv')
+id_problem_path = os.path.join(DIR_REPO, "zqy", "dataset", "reference.csv")
 data = pd.read_csv(id_problem_path)
 output = data.copy()
-output.drop(columns=['problem'], inplace=True)
+output.drop(columns=["problem"], inplace=True)
 for i in range(repeat):
-    output[f'A{i+1}'] = None
+    output[f"A{i+1}"] = None
+
 
 def predict(data):
     global output
     for prob_i in range(data.shape[0]):
-        question = data.loc[prob_i, 'problem']
+        question = data.loc[prob_i, "problem"]
         for i in range(repeat):
             time1 = time()
             response = solver.solve(question, reasoning_type="CoT")
             # trim the prediction
             ans = pred2int(response)
-            output.loc[prob_i, f'A{i+1}'] = ans
-            output.to_csv(os.path.join(DIR_REPO, 'zqy', 'temp', 'output.csv'), index=False) 
+            output.loc[prob_i, f"A{i+1}"] = ans
+            output.to_csv(
+                os.path.join(DIR_REPO, "zqy", "dataset", "output.csv"), index=False
+            )
             time2 = time()
             print(f"Problem id: {prob_i}")
             print(f"repeat times: {i+1}")
-            print(f"time expenditure: ", time2-time1)
-            
+            print(f"time expenditure: ", time2 - time1)
+
+
 predict(data)
-
-
-
